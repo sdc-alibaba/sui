fdaeacc6d7c2d9cfb43266391d17b02a6ad41369
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2014-01-24 11:27:55 +0800

    add jquery

--- js/requirejs-config.js
+++ js/requirejs-config.js
@@ -1,16 +1,8 @@
 (function() {
   requirejs.config({
     paths: {
-      "jquery": "lib/jquery-1.10.2",
-      "backbone": "lib/backbone",
-      "underscore": "lib/underscore",
       "almond": "lib/almond"
     },
-    shim: {
-      "backbone": {
-        deps: ["underscore", "jquery"]
-      }
-    }
   });
 
 }).call(this);
