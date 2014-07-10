0cb822aeb5349f1c5441d2d66db3da7d038bdeba
夏苒 <qiuyan.mqy@alibaba-inc.com>
2014-07-10 19:04:48 +0800

    index

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,50 +1,80 @@
 $(function() {
   'use strict';
+    var scrolltop = $(".content").scrollTop();
+    var lastScrollTop = 0;
+    var scrolling = false;
+    var num = 1;
+    $(document).ready(function(){
+      $(".main").onepage_scroll({
+        sectionContainer: "section",
+        responsiveFallback: 600,
+        loop: true,
+        afterMove: function(index) {
+          if(index===2&&num===1){
+            go()
+          }
+        }
+      });
+    });
 
-  var windowHeight = $(window).height();
-  var scrolltop = $(".content").scrollTop();
-  var lastScrollTop = 0;
-  var scrolling = false;
-
-  $(window).resize(function(){
-    windowHeight = $(window).height();
-  })
-  $(window).scroll(scrollto);
-
-
-  function scrollto() {
-      console.log(scrolling);
-      if(scrolling) {
-        lastScrollTop = $(this).scrollTop();
-        return true;
-      }
-      scrolling = true;
-      var st = $(this).scrollTop();
-      if (st > lastScrollTop){
-        console.log('scroll top');
-        $('body,html').animate({ scrollTop: windowHeight-59 }, 200,function(){
-          scrolling = false;
-          console.log("end");
-        });
-      } else {
-        console.log('scroll bottom');
-        $('body,html').animate({ scrollTop: -(windowHeight-59) }, 200,function(){
-          scrolling = false;
-          console.log("end");
-        });
-      }
-      lastScrollTop = st;
+    if ($(".features").length > 0) {
+      var defaultOpts = { interval: 5000, fadeInTime: 300, fadeOutTime: 200 };
+      var _bodies = $(".features .j-banner");
+      var _count = _bodies.length;
+      var _current = 0;
+      var _intervalID = null;
+      var stop = function () { window.clearInterval(_intervalID); };
+      var slide = function (opts) {
+          if (opts) {
+              _current = opts.current || 0;
+          } else {
+              _current = (_current >= (_count - 1)) ? 0 : (++_current);
+          };
+          _bodies.filter(":visible").fadeOut(defaultOpts.fadeOutTime, function () {
+              _bodies.eq(_current).fadeIn(defaultOpts.fadeInTime);
+              _bodies.removeClass("cur").eq(_current).addClass("cur");
+          });        
+      };
+      // var itemMouseOver = function (target, items) {
+      //     stop();
+      //     var i = $.inArray(target, items);
+      //     slide({ current: i });
+      // };
+      // _bodies.hover(stop, go);
   }
-  var onScroll = function() {
-    $('.jumbotron').each(function() {
-      var scrollTop = $(document).scrollTop()
-      var $this = $(this)
-      var offsetBottom = windowHeight - ($this.offset()['top'] - scrollTop)
-      if(offsetBottom > Math.min(windowHeight / 2, 300)) {
-        $this.addClass('inview')
-      }
-    })
-  }
-  $(document).scroll(onScroll)
-  setTimeout(onScroll, 1000)
+ 
+
+
+ 
+  function go () {
+    stop();
+    _intervalID = window.setInterval(function () { slide(); }, defaultOpts.interval);
+    $("[value='zoomInLeft']").parent().removeClass("disappear");
+    $("[value='zoomInLeft']").addClass('animated zoomInLeft')
+    setTimeout(function(){
+      $("[value='rollIn']").parent().removeClass("disappear");
+      $("[value='rollIn']").addClass('animated rollIn');
+    },1000);
+    setTimeout(function(){
+      $("[value='fadeInUpBig']").parent().removeClass("disappear");
+      $("[value='fadeInUpBig']").addClass('animated fadeInUpBig');
+    },2000);
+    setTimeout(function(){
+      $("[value='rotateIn']").parent().removeClass("disappear");
+      $("[value='rotateIn']").addClass('animated rotateIn');
+    },3000);
+    setTimeout(function(){
+      $("[value='swing']").addClass('animated swing');
+    },4000);
+    setTimeout(function(){
+      $("[value='zoomInDown']").addClass('animated zoomInDown');
+      $("[value='zoomInRight']").addClass('animated zoomInRight');
+      $("[value='zoomInUp']").addClass('animated zoomInUp');
+    },5000)
+    setTimeout(function(){
+      $(".animated").removeClass();
+      num = 0;
+    },10000);
+  };
 })
+
