5a29a1a86490031692fc92b8797fe9814f54b7cf
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2014-09-14 13:11:28 +0800

    build

--- .package/js/sui.js
+++ .package/js/sui.js
@@ -4610,9 +4610,24 @@
 //msgs组件添加叉叉关闭功能
 !function ($) {
   $(document).on('click.msgs', '[data-dismiss="msgs"]', function (e) {
-    e.preventDefault()
-    $(this).parents('.sui-msg').remove();
+    e.preventDefault();
+    var $this = $(this),
+        $msg = $this.parents('.sui-msg').remove();
+    
+    var id = $msg.attr("id");
+    if(id && $msg.hasClass("remember")) {
+      localStorage.setItem("sui-msg-" + id, 1);
+    }
   })
+
+  $(function() {
+    $(".sui-msg.remember").each(function() {
+      var $this = $(this);
+      var id = $this.attr("id");
+      if(!id) return;
+      localStorage.getItem("sui-msg-" + id) || $this.show();
+    });
+  });
 }(window.jQuery);
 
 },{}],11:[function(require,module,exports){
